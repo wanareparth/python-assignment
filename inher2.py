de308f9965552baf14d0d502dfca7af3176f61f6
wanareparth <wanareparth@gmail.com>
2026-04-26 03:07:13 +0530

    Create inher2.py

---
+++ inher2.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show_person_details(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
